9ca82902326d2e858f300b71c8d0665a2c4d92d1
Chris Roskelley <RoskelleyCJ@familysearch.org>
2019-10-31 08:00:27 -0600

    Reversing didn't work
    For some reason we need the apigateway id
    instead of the pretty name.
    
    Additionally, needed some more instrumentation.

--- sysps-job-start.sh
+++ sysps-job-start.sh
@@ -11,12 +11,12 @@ SYSTEM_NAME=$8
 BLUEPRINT_VERSION_URL=$9
 
 # https://yoozc195t9.execute-api.us-east-1.amazonaws.com/v1/check/dougireton-paas-tutorial/dev/jobs
-POST_JOB_URL=https://api.fsdpt.org/service_provisioning/$PHASE/$BLUEPRINT_NAME/$SYSTEM_NAME/jobs
-#POST_JOB_URL=https://yoozc195t9.execute-api.us-east-1.amazonaws.com/v1/$PHASE/$BLUEPRINT_NAME/$SYSTEM_NAME/jobs
+#POST_JOB_URL=https://api.fsdpt.org/service_provisioning/$PHASE/$BLUEPRINT_NAME/$SYSTEM_NAME/jobs
+POST_JOB_URL=https://yoozc195t9.execute-api.us-east-1.amazonaws.com/v1/$PHASE/$BLUEPRINT_NAME/$SYSTEM_NAME/jobs
 
 echo "Submitting job: $POST_JOB_URL"
 
-HTTP_STATUS=`curl -s -w "%{http_code}" \
+curl -s -w "Job submitted:  http_code=%{http_code}\n" \
   $POST_JOB_URL \
   -H 'Accept: */*' \
   -H 'Accept-Encoding: gzip, deflate' \
@@ -35,9 +35,7 @@ HTTP_STATUS=`curl -s -w "%{http_code}" \
         \"blueprintName\": \"$BLUEPRINT_NAME\",
         \"blueprintVersionUrl\": \"$BLUEPRINT_VERSION_URL\",
         \"definition\":{}
-    }}" -o post-job`
-
-echo "http status: $HTTP_STATUS"
+    }}" -o post-job
 
 JOBID=`jq -r -e '.|.id' post-job`
 if [ $? -ne 0 ]
@@ -51,12 +49,13 @@ GET_JOB_URL=$POST_JOB_URL/$JOBID
 GET_JOB_STATUS_URL=$GET_JOB_URL/status
 
 echo "Checking status on '$JOBID'"
+TRY_ATTEMPT=1
 STATUS="INPROGRESS"
 while [ $STATUS != "CLOSED" ]
 do
   sleep 3
 
-  curl -s -X GET \
+  curl -s -G -w "Job status attempt:  http_code=%{http_code}\n" \
   $GET_JOB_STATUS_URL \
   -H 'Accept: */*' \
   -H 'Accept-Encoding: gzip, deflate' \
@@ -64,16 +63,16 @@ do
   -H 'Cache-Control: no-cache' \
   -H 'Connection: keep-alive' \
   -H 'Content-Type: application/json' \
-  -H 'Host: api.fsdpt.org' \
   -H 'User-Agent: roskelleycj/1.0 (manual)' \
-  -H 'cache-control: no-cache' >status
+  -H 'cache-control: no-cache' -o status-$TRY_ATTEMPT
 
-  STATUS=`jq -r '.|.executionStatus' status`
+  STATUS=`jq -r '.|.executionStatus' status-$TRY_ATTEMPT`
+  ((TRY_ATTEMPT=TRY_ATTEMPT+1))
   echo "Job status: '$STATUS'"
 done
 
-echo "Reporting status for job"
-curl -s -X GET \
+echo "Getting status for job"
+curl -s -G -w "Reporting status for job:  http_code=%{http_code}\n" \
   $GET_JOB_URL \
   -H 'Accept: */*' \
   -H 'Accept-Encoding: gzip, deflate' \
@@ -81,9 +80,8 @@ curl -s -X GET \
   -H 'Cache-Control: no-cache' \
   -H 'Connection: keep-alive' \
   -H 'Content-Type: application/json' \
-  -H 'Host: api.fsdpt.org' \
   -H 'User-Agent: roskelleycj/1.0 (manual)' \
-  -H 'cache-control: no-cache' >report
+  -H 'cache-control: no-cache' -o report
 
 
 echo "Found warnings: `jq '.|.warnings' report`"
